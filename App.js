34919be6e1b9e2dd4f09ecf0b68fd081402a5213
SeaFalcon <seafalcon323@gmail.com>
2020-09-26 23:48:46 +0900

    cloning weather app by nomads lecture

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+import Loading from './Loading';
+import Weather from './Weather';
+
+const API_KEY = 'bd20651f0d8fc6b40e13d67c3cb92f3b';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [state, setState] = useState({
+    isLoading: true,
+    temp: null,
+    condition: null,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const { isLoading, temp, condition } = state;
+
+  const getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+
+    setState({
+      isLoading: false,
+      temp,
+      condition: weather[0].main,
+    });
+  }
+
+  useEffect(() => {
+    async function getLocation() {
+      try {
+        await Location.requestPermissionsAsync();
+
+        const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+
+        getWeather(latitude, longitude);
+      } catch (error) {
+        Alert.alert("Can't find you.", "So sad");
+      }
+    };
+
+    getLocation();
+  }, []);
+
+  if (!isLoading) return <Weather temp={Math.round(temp)} condition={condition} />
+  return <Loading />;
+}
